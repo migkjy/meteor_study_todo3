8fc7a050e805f6490e31d60509aa95138f6bfbab
migkjy <migkjy@naver.com>
2017-11-06 16:33:27 +0900

    second commit

--- client/main.js
+++ client/main.js
@@ -1,22 +1,37 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { Notes } from '../lib/collections.js';
+import { Accounts } from 'meteor/accounts-base';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+
+// Accounts config
+Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_ONLY',
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+Template.body.helpers({
+  notes() {
+    return Notes.find({});
   },
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+Template.add.events({
+  'submit .add-form': function () {
+    event.preventDefault();
+
+    // get input value
+    const target = event.target;
+    const text = target.text.value;
+
+    // insert note into collection
+    Notes.insert({
+      text,
+      createAt: new Date(),
+      owner: Meteor.userId(),
+      userName: Meteor.user().username,
+    });
   },
 });
+
+Template.note.e
